0b07b04b4e5e78c1450810b05e2a6b132de76d4f
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-09-03 14:31:36 -0700

    index.scss

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    const canvas = document.getElementById('octet-game');
+
+    const ctx = canvas.getContext("2d");
+    canvas.fillStyle = "skyblue";
+    canvas.fillRect(0, 0, "700", "700");
+})
